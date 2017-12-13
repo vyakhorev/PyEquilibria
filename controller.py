ae7bd10af31a5ab78e3502373830a14540e41f5c
Alexey_Vyakhorev <omonyomony@gmail.com>
2017-12-13 17:35:04 +0300

    A test commit to test branch

--- controller.py
+++ controller.py
@@ -1,14 +1,19 @@
 
 
+
 # This is a third test with git - now console in action!
 # Added something to check status
+
+# this should be in test branch
 
 from simcubes.behaviours.basebehaviour import iter_threads_in_holders
 from simcubes.simcore import cSimEnvironment
 
 from levelgenerator.plain import generate_test_landscape
 
+
 "Another commit to be done"
+
 
 class cSimulationController:
     '''
